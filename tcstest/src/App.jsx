5a8fbd955b4f10341a2fa756c06358430ed67daa
CrisGarcis <jhonier491@gmail.com>
2022-02-23 14:12:24 -0500

    finished feature

--- tcstest/src/App.jsx
+++ tcstest/src/App.jsx
@@ -1,42 +1,48 @@
 
-import React, { useEffect, useContext } from "react";
+import React, {  useContext } from "react";
 import "./App.css";
 import { Form, Field } from 'react-final-form';
 import {
   SecurityContextProvider,
-  SecurityContext
+  SecurityContext,
+  STATUS_LOADING
 } from "./contexts/SecurityContext";
 
-const AppRender = ({ match }) => {
-  const [{ }, {validateText }] = useContext(SecurityContext)
+const AppRender = () => {
+  const [{ repeats }, { validateText,required }] = useContext(SecurityContext)
 
   return (
-    <div className="flex">
+    <div className="container">
 
-   <Form
-    onSubmit={validateText}
-  
-    render={({ handleSubmit }) => (
-      <form onSubmit={handleSubmit}>
-        <h2>Simple Default Input</h2>
-       
-       
-        <Field
-          name="bio"
-          render={({ input, meta }) => (
-            <div>
-              <label>Bio</label>
-              <textarea {...input} />
-              {meta.touched && meta.error && <span>{meta.error}</span>}
-            </div>
-          )}
-        />
+      <Form
+        onSubmit={validateText}
 
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <h2>Ingrese un texto</h2>
+            <label>Se buscaran las palabras repetidas</label>
+            <Field
+            validate={required}
+              name="text_validate"
+              render={({ input, meta }) => (
+                <div>
+                  <textarea className="form-control" {...input} />
+                  {meta.touched && meta.error && <span>{meta.error}</span>}
+                </div>
+              )}
+            />
+            <br></br> <button className="btn btn-primary" type="submit">Validar</button>
+          </form>
+        )}
+      />
 
-        <button type="submit">Submit</button>
-      </form>
-    )}
-  />
+      {repeats.status===STATUS_LOADING&&"Buscando coincidencias"}
+      {repeats.repeats.map((r,i) => {
+        return (<div key={i}>
+
+         <strong>{r[0]}</strong>  se repite <strong>{r[1]}</strong> veces
+        </div>)
+      })}
     </div>
   );
 
--- tcstest/src/contexts/SecurityContext.js
+++ tcstest/src/contexts/SecurityContext.js
@@ -1,10 +1,42 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+export const STATUS_LOADED = "LOADED";
+export const STATUS_LOADING = "LOADING";
+
+const initialState = {
+    status: STATUS_LOADED,
+    repeats: []
+}
 export const SecurityContext = createContext([]);
 export const SecurityContextProvider = ({ children }) => {
-    const validateText=()=>{
-        
+    const [repeats, setrepeats] = useState(initialState);
+    const validateText = ({ text_validate }) => {
+        if (repeats.status === STATUS_LOADED) {
+            setrepeats({ ...repeats, status: STATUS_LOADING });
+            //remover caracteres espaciales
+            text_validate = removeSpecialCaracters(text_validate);
+            //convertir a minùscula y array
+            let arrayLower = text_validate.toLowerCase().split(/[\s\.,]+/g);;
+
+            let response = {};
+            arrayLower.forEach(text => {
+                response[text] = (response[text] || 0) + 1;
+            });
+            setrepeats({ ...repeats, status: STATUS_LOADED, repeats: Object.entries(response).filter(a => a[1] > 1) });
+        }
+
     }
-    return (<SecurityContext.Provider value={[{}, {validateText}]}>
+    const removeSpecialCaracters = (text_validate) => {
+        var specialChars = "!@#$^&%*()+=-[]\/{}|:<>?,.";
+        for (var i = 0; i < specialChars.length; i++) {
+            text_validate = text_validate.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+        }
+        return text_validate;
+
+    }
+    const required = (value) => (value ? undefined : "Ingrese un texto")
+
+
+    return (<SecurityContext.Provider value={[{ repeats }, { validateText, required }]}>
         {children}
     </SecurityContext.Provider>)
 }
